71eb93b66494ef91b85442b20e25d351bec3ad72
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2020-12-31 16:42:12 +0900

    Update solution.py

--- 프로그래머스/hash/전화번호 목록/solution.py
+++ 프로그래머스/hash/전화번호 목록/solution.py
@@ -20,3 +20,18 @@ def solution(phoneBook):
         if p2.startswith(p1):
             return False
     return True
+
+
+#hash사용
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+    return answer
