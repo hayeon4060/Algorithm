f0a1888a401a1a715115ad0d36c6c193749163b5
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2020-12-31 16:19:55 +0900

    Update solution.py

--- 프로그래머스/hash/전화번호 목록/solution.py
+++ 프로그래머스/hash/전화번호 목록/solution.py
@@ -1,9 +1,9 @@
 def solution(phone_book):
     answer = True
     phone_book.sort()
-    for i in range(len(phone_book)):
-        for j in range(len(phone_book)-1-i):
-            if phone_book[i].find(phone_book[j+1+i])==0:
+    for i in range(0,len(phone_book)):
+        for j in range(i+1,len(phone_book)):
+            if phone_book[j].find(phone_book[i])==0:
                 return False
     
     return answer
