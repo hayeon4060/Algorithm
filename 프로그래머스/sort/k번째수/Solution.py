9569c6506d6190f83c45249d806620ffcdbfc2ba
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-01-03 22:21:05 +0900

    Update Solution.py

--- 프로그래머스/sort/k번째수/Solution.py
+++ 프로그래머스/sort/k번째수/Solution.py
@@ -11,3 +11,17 @@ def solution(array, commands):
         tmp.sort()
         answer.append(tmp[i[2]-1])
     return answer
+
+
+#다른사람 풀이1
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+
+
+#다른사람 풀이2
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i,j,k = command
+        answer.append(list(sorted(array[i-1:j]))[k-1])
+    return answer
