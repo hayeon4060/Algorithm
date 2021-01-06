6977739976bc48cbe1b2c2e87d201e05fe36aed1
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-01-06 21:16:56 +0900

    Update Solution.py

--- 프로그래머스/sort/가장 큰 수/Solution.py
+++ 프로그래머스/sort/가장 큰 수/Solution.py
@@ -18,3 +18,18 @@ def solution2(numbers):
     print("numbers: {}".format(numbers))
 
     return str(int(''.join(numbers)))
+
+
+#다른사람 풀이2
+import functools
+
+def comparator(a,b):
+    t1 = a+b
+    t2 = b+a
+    return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+
+def solution(numbers):
+    n = [str(x) for x in numbers]
+    n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
+    answer = str(int(''.join(n)))
+    return answer
