af0417bcf59573918b3684444c98bd7a1573cd5f
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-01-09 23:35:16 +0900

    Update Solution.py

--- 프로그래머스/Stack and Queue/주식가격/Solution.py
+++ 프로그래머스/Stack and Queue/주식가격/Solution.py
@@ -16,3 +16,40 @@ def solution(prices):
             answer.append(where-stand)
             
     return answer
+
+
+
+#2차 
+def solution(prices):
+    p_return=[]
+
+    for i in range(0,len(prices)):
+        cut=0
+        for j in range(i+1,len(prices)):
+            if prices[i] > prices[j]:
+                p_return.append(j-i)
+                cut=1
+                break
+        if cut==0:
+            p_return.append(j-i)
+    return p_return
+
+
+#queue사용
+from collections import deque
+def solution(prices):
+    answer = []
+    prices = deque(prices)
+    while prices:
+        c = prices.popleft()
+
+        count = 0
+        for i in prices:
+            if c > i:
+                count += 1
+                break
+            count += 1
+
+        answer.append(count)
+
+    return answer
