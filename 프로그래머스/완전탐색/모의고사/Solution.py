7c60802e698079af244b7f0be4b2e91ed4e0be6b
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-01-14 16:28:58 +0900

    Update Solution.py

--- 프로그래머스/완전탐색/모의고사/Solution.py
+++ 프로그래머스/완전탐색/모의고사/Solution.py
@@ -17,3 +17,17 @@ def solution(answers):
             answer.append(i+1)
 
     return answer
+
+
+#다른사람 풀이1
+def solution(answers):
+    p = [[1, 2, 3, 4, 5],
+         [2, 1, 2, 3, 2, 4, 2, 5],
+         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    s = [0] * len(p)
+
+    for q, a in enumerate(answers):
+        for i, v in enumerate(p):
+            if a == v[q % len(v)]:
+                s[i] += 1
+    return [i + 1 for i, v in enumerate(s) if v == max(s)]
