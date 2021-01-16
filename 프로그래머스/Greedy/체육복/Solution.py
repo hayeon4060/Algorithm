91a1834a7c60d5e8f4851a9247b91abb2969aa91
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-01-16 22:59:43 +0900

    Update Solution.py

--- 프로그래머스/Greedy/체육복/Solution.py
+++ 프로그래머스/Greedy/체육복/Solution.py
@@ -1,1 +1,14 @@
-.
+#1차 
+def solution(n, lost, reserve):
+    
+    answer=[i+1 for i in lost if i+1 in reserve ]
+    answer+=list(map(lambda x: x-2, answer))
+    answer=list(set(answer))
+    answer=[i for i in answer if i in reserve]
+    print(answer)
+    if len(answer)>len(lost):
+        avail=len(lost)
+    else:
+        avail=len(answer)
+    return n-len(lost)+avail
+    #return n-len(reserve)+len(answer_set)
