cea805de9b1a05ad9b5aa72c60f2019f3371e0d7
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-01-18 00:00:05 +0900

    Update Solution.py

--- 프로그래머스/Greedy/체육복/Solution.py
+++ 프로그래머스/Greedy/체육복/Solution.py
@@ -15,7 +15,7 @@ def solution(n, lost, reserve):
     
     
     
-#2차
+#2차 리스트를 만들어, 여벌있는 사람은 +1, 사라진 사람은 -1 하여 2개씩 더함
 def solution(n, lost, reserve):
 
     tmp=[0 for i in range(n+1)]
@@ -23,6 +23,36 @@ def solution(n, lost, reserve):
         tmp[i]=1
     for i in lost:
         tmp[i]=-1
+    i=1
+    no=0
+    print(tmp)
+    while i<=n :
+        j=i+1
+        if i==n:
+            if tmp[i]==-1:
+                no+=1
+            break
+        if tmp[i]!=0:
+            if tmp[i]+tmp[j]==0:
+                i+=2
+            elif tmp[i]+tmp[j]<0:
+                i+=1
+                no+=1
+            else:
+                i+=1
+        else:
+            i+=1
+    return n-no
+
+
+#3차 성공 (여벌 가져온 사람이 도둑맞을 수도 있음)
+def solution(n, lost, reserve):
+
+    tmp=[0 for i in range(n+1)]
+    for i in reserve:
+        tmp[i]=1
+    for i in lost:
+        tmp[i]-=1
     i=1
     no=0
     print(tmp)
