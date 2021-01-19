8376c7b0bc0319130ff72d42400da34b00833450
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-01-19 19:30:03 +0900

    Create Solution.py

---
+++ 프로그래머스/Greedy/큰 수 만들기/Solution.py
@@ -0,0 +1,21 @@
+def solution(number, k):
+    answer = ''
+    tmp=[]
+    tmp.append(0)
+    k+=1
+    j=0
+    for i in range(len(number)):
+        if number[i]>tmp[j] and k>0:
+            tmp[j]=number[i]
+            k-=1
+        elif number[i]<=tmp[j]:
+            tmp.append(number[i])
+            j+=1
+        if k==0:
+            for a in range(i, len(number)):
+                tmp.append(number[a])
+            break
+            
+    print(tmp)
+    
+    return answer
