897e6470269ad6a025260dfc7ec5cba51e47d4e8
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-01-21 14:22:28 +0900

    Update Solution.py

--- 프로그래머스/Greedy/큰 수 만들기/Solution.py
+++ 프로그래머스/Greedy/큰 수 만들기/Solution.py
@@ -20,3 +20,27 @@ def solution(number, k):
     print(tmp)
     
     return answer
+#2차 테스트 3번에서 
+def solution(number, k):
+    answer = ''
+    number=list(map(lambda x: int(x), list(number)))
+    tmp=[]
+    tmp.append(0)
+    k+=1
+    j=0
+    for i in range(len(number)):
+        print(i)
+        if number[i]>tmp[j] and k>0:
+            tmp[j]=number[i]
+            k-=1
+        elif number[i]<=tmp[j]:
+            tmp.append(number[i])
+            j+=1
+        elif k==0 and i<len(number):
+            for a in range(i, len(number)):
+                tmp.append(number[a])
+            break
+        print(tmp, k, i)
+
+    
+    return answer
