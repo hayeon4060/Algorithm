f47b2143b715d1cde4a3d1303c017316162d6bcf
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-02-01 13:56:25 +0900

    Create Solution.py

---
+++ 프로그래머스/깊이,너비 우선탐색/타겟 넘버/Solution.py
@@ -0,0 +1,12 @@
+def solution(numbers, target):
+    answer=0
+    tmp = (sum(numbers)-target)/2
+    
+    for i in numbers:
+        j=i
+        
+        while j<tmp:
+            j
+    
+    
+    return answer
