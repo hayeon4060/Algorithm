17e34f397956284b91e68e9783572a6898bfcd65
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-02-15 15:54:48 +0900

    Create Solution.py

---
+++ 프로그래머스/완전탐색/소수찾기/Solution.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+def solution(numbers):
+    answer = 0
+    lst=[]
+    for i in range(1, len(numbers)+1):
+        lst+=list(permutations(list(numbers),i))
+    for i in range(len(lst)):
+        lst[i]=int("".join(lst[i]))
+    lst=list(set(lst))
+    for i in lst:
+        if prime(i):
+            answer+=1
+    return answer
+    
+def prime(a):
+    for i in range(2, a):
+        if a%i==0:
+            return False
+        else: return True
