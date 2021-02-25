f11b2e45c1471c79e06f2ff00494b3058fc0bce2
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-02-25 16:17:43 +0900

    Update Solution.py

--- 프로그래머스/완전탐색/소수찾기/Solution.py
+++ 프로그래머스/완전탐색/소수찾기/Solution.py
@@ -26,7 +26,7 @@ def prime(a):
 
 
 
-#2차 시도
+#2차 시도(성공)
 from itertools import permutations
 import math
 def solution(numbers):
@@ -51,3 +51,15 @@ def prime(n):
         if n % i == 0:
             return False
     return True
+
+
+#다른사람 풀이
+from itertools import permutations
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))
+    return len(a)
