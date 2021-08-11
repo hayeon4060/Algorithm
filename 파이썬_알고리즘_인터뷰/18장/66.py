0be53e7c50b0fda663c2539fcfd8e1a2dcbdbe22
LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
2021-08-11 15:10:21 +0900

    Create 66.py

---
+++ 파이썬_알고리즘_인터뷰/18장/66.py
@@ -0,0 +1,11 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        try:
+            found = nums.index(target)
+            
+        except: 
+            found =-1
+            
+            
+        return found
